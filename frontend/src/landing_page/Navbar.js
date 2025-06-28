879276958fd90cba6791d26608de497b52d5078b
jainiahh <jainishpatel1782005@gmail.com>
2025-06-28 08:34:06 +0530

    set Navbar Colmpelte

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,18 +1,20 @@
 import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-light border-bottom">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg bg-light border-bottom ">
+      <div className="container p-2">
+        <a className="navbar-brand mx-5" href="#">
           <img
             src="media\images\logo.svg"
             alt="logo"
-            style={{ width: "120px" }}
+            style={{ width: "140px" }}
           />
         </a>
+
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -20,38 +22,39 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <form class="d-flex" role="search">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">
-                  Signup
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Poducts
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Support
-                </a>
-              </li>
-            </ul>
-          </form>
+        <div
+          className="collapse navbar-collapse justify-content-center ml-5"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav text-center">
+            <li className="nav-item first">
+              <a className="nav-link" aria-current="page" href="#">
+                Signup
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Poducts
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Support
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
